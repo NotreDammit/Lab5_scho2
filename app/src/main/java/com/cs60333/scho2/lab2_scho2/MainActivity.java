4976d346927ec44f5e76c0846f40091145b2768a
Samuel Cho <scho2@nd.edu>
2017-02-03 20:07:35 -0500

    Initial commit

---
+++ app/src/main/java/com/cs60333/scho2/lab2_scho2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cs60333.scho2.lab2_scho2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
